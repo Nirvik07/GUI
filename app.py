0adee3608c634b36cc7a0d4af58033dfb4ff9384
suman-tewary <suma.tewary.2000@gmail.com>
2023-04-18 20:59:25 +0530

    analize page updated

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import customtkinter
 from customtkinter import filedialog
 import os
 from PIL import Image
+import PyPDF2
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import string
+import re
 
 customtkinter.set_appearance_mode("light")
 
@@ -91,23 +98,28 @@ class App(customtkinter.CTk):
         self.analyze_frame = customtkinter.CTkFrame(
             self, corner_radius=0, fg_color="transparent")
         self.analyze_frame.grid_columnconfigure(0, weight=1)
-        # self.analyze_frame.grid_rowconfigure(1, weight=1)
         self.analyze_title_label = customtkinter.CTkLabel(
-            self.analyze_frame, text="Analyze Page",font=customtkinter.CTkFont(size=20, weight="bold"))
+            self.analyze_frame, text="Analyze Page", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.analyze_title_label.grid(
             row=0, column=0, pady=10, padx=10, sticky="new")
-        
-        self.analyze_upload_frame=customtkinter.CTkFrame(self.analyze_frame)
-        self.analyze_upload_frame.grid(row=1,column=0,pady=10, padx=10, sticky="new")
-        # upload frame
-        self.file_upload_btn=customtkinter.CTkButton(self.analyze_upload_frame,text="Upload PDF",command=self.upload_file)
-        self.file_location_label=customtkinter.CTkLabel(self.analyze_upload_frame,text="No File Selected",text_color="red")
-        self.analyze_file_btn=customtkinter.CTkButton(self.analyze_upload_frame,text="Analyze File")
-        self.file_upload_btn.grid(row=0, column=0, pady=10, padx=10, sticky="nw")
-        self.file_location_label.grid(row=0,column=1,pady=10, padx=10, sticky="nw")
-        self.analyze_file_btn.grid(row=1,column=1,pady=10, padx=10, sticky="nw")
-
-
+# Analize Upload Frame
+        self.analyze_upload_frame = customtkinter.CTkFrame(self.analyze_frame)
+        self.analyze_upload_frame.grid(
+            row=1, column=0, pady=10, padx=10, sticky="new")
+        self.file_upload_btn = customtkinter.CTkButton(
+            self.analyze_upload_frame, text="Upload PDF", command=self.upload_file)
+        self.file_location_label = customtkinter.CTkLabel(
+            self.analyze_upload_frame, text="No File Selected", text_color="red")
+        self.analyze_file_btn = customtkinter.CTkButton(
+            self.analyze_upload_frame, text="Analyze File", command=self.analyze_btn)
+        self.file_upload_btn.grid(
+            row=0, column=0, pady=10, padx=10, sticky="nw")
+        self.file_location_label.grid(
+            row=0, column=1, pady=10, padx=10, sticky="nw")
+# file analyze frame
+        self.analyze_file_frame = customtkinter.CTkFrame(self.analyze_frame)
+        self.analyze_doc_label = customtkinter.CTkLabel(
+            self.analyze_file_frame)
 
 
 ############################################################################################
@@ -165,6 +177,29 @@ class App(customtkinter.CTk):
 
 # active the home button start
         self.select_frame_by_name("home")
+
+
+# analize frame change
+
+    def analyze_frame_switch(self, name):
+        if name == "analyze":
+            self.analyze_file_frame.grid(
+                row=1, column=0, pady=10, padx=10, sticky="new")
+        else:
+            self.analyze_file_frame.grid_forget()
+        if name == "upload":
+            self.analyze_upload_frame.grid(
+                row=1, column=0, pady=10, padx=10, sticky="new")
+        else:
+            self.analyze_upload_frame.grid_forget()
+
+    def analyze_btn(self):
+        self.analyze_frame_switch("analyze")
+        global words
+        words=self.file_preprocese()
+        self.analyze_doc_label.configure(
+            text="Crime Type Find :"+str(self.extract_crime_names(words))+"\n IPC Find "+str(self.extract_charges_names(words))+"\n Found evidence related to "+str(self.assault_weapon()))
+        self.analyze_doc_label.grid(row=0, column=0)
 
 # function for active coler in nav
 
@@ -239,11 +274,102 @@ class App(customtkinter.CTk):
         self.select_frame_by_name("eda")
 
     def upload_file(self):
-        file_path=filedialog.askopenfilename(filetypes=[("PDF File",'.pdf')])
-        if(file_path):
-            self.file_location_label.configure(text="File Selected: "+file_path,text_color="black")
+        global file_path
+        file_path = filedialog.askopenfilename(
+            filetypes=[("PDF File", '.pdf')])
+        if (file_path):
+            self.file_location_label.configure(
+                text="File Selected: "+file_path, text_color="black")
+            self.analyze_file_btn.grid(
+                row=1, column=1, pady=10, padx=10, sticky="nw")
 
+    def analyze(self):
+        # self.analyze_upload_frame.grid(
+        # row=1, column=0, pady=10, padx=10, sticky="new")
+        self.analyze_upload_frame.grid_forget()
 
+    def stem_list(self, lst):
+        stemmed_lst = []
+        for word in lst:
+            stemmed_word = PorterStemmer().stem(word)
+            stemmed_lst.append(stemmed_word)
+        return stemmed_lst
+
+    def file_preprocese(self):
+        pdf_file = open(file_path, 'rb')
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        global pdf_text
+        pdf_text = ''
+        for page in range(len(pdf_reader.pages)):
+            page_obj = pdf_reader.pages[page]
+            pdf_text += page_obj.extract_text()
+        tokens = nltk.word_tokenize(pdf_text)
+        lowercase_words = [word.lower() for word in tokens]
+        stop_words = set(stopwords.words('english'))
+        words = [word for word in lowercase_words if word not in stop_words]
+        stemmed_words = self.stem_list(words)
+        # create an instance of the WordNetLemmatizer
+        lemmatizer = WordNetLemmatizer()
+        # lemmatize each word in the list
+        lemmatized_words = [lemmatizer.lemmatize(
+            word) for word in stemmed_words]
+        # Create a string containing all punctuation characters
+        punctuation_chars = string.punctuation
+        # Use a list comprehension to filter out the punctuations
+        my_list_without_punctuations = [
+            word for word in lemmatized_words if word not in punctuation_chars]
+        return my_list_without_punctuations
+
+    def extract_crime_names(self, strings):
+        # A list of common crime names to search for
+        crime_names = ["theft", "robbery", "assault", "murder", "fraud", "burglary", "kidnapping",
+                       "arson", "Criminal force", "Outrageof modesty", "Assault of woman", "rape"]
+
+        # Compile a regular expression pattern to match any of the crime names
+        pattern = re.compile("|".join(crime_names), re.IGNORECASE)
+
+        # Iterate over each string in the input list and search for matches
+        matches = []
+        for string in strings:
+            match = pattern.search(string)
+            if match:
+                matches.append(match.group(0))
+
+        # Return the list of matches
+        return matches
+
+    def extract_charges_names(self, strings):
+        # A list of common crime names to search for
+        crime_names = ["363", "366", "376", "506",
+                       "509", "indian penal code", "ipc"]
+
+        # Compile a regular expression pattern to match any of the crime names
+        pattern = re.compile("|".join(crime_names), re.IGNORECASE)
+
+        # Iterate over each string in the input list and search for matches
+        matches = []
+        for string in strings:
+            match = pattern.search(string)
+            if match:
+                matches.append(match.group(0))
+
+        # Return the list of matches
+        return matches
+    def assault_weapon(self):
+        keywords = ['murder', 'weapon', 'bloodstain', 'DNA','blood', 'fingerprints','Witness']
+        for keyword in keywords:
+            for item in words:
+                if re.search(r'\b{}\b'.format(keyword), item, flags=re.IGNORECASE):
+                    return item
+                
+    # def find_date(self):
+    #     judge_name_pattern = r"The Hon\'ble Justice [A-Z][a-z]+\s[A-Z][a-z]+\s?[A-Z]?[a-z]+"
+    #     match = re.search(judge_name_pattern, pdf_text)
+    #     if match:
+    #         judge_name = match.group(1)
+    #         return ("Judge name:", judge_name)
+    #     else:
+    #         return ("Judge name not found.")
 if __name__ == "__main__":
     app = App()
     app.mainloop()
